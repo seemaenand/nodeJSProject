087222baa052ade337e5f003daf39d28391f5167
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-07-24 09:31:12 +0100

    created a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,8 @@
 module.exports.home=function(req,res){
-    return res.end('<h1>Express is up for running the controller</h1>')
+    // return res.end('<h1>Express is up for running the controller</h1>');
+    // now we want to render an ejs file
+
+    return res.render('home', {
+        title :"Home"
+    });
 };
