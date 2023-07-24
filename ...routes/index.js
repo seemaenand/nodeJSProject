636d198e90e5ffb2c795a2b94284eeb71143410f
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-07-24 09:09:32 +0100

    added controller for user profile and post

--- ...routes/index.js
+++ ...routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home );
+// router.use('/users', require('./users'));
+router.use('/users', require('./users'));
+
 
 // console.log("Router is set");
 
---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+module.exports.profile=function(req,res){
+    res.end('<h1>User profile</h1>');
+}
+
+module.exports.post=function(req,res){
+    res.end('<h1>User post</h1>');
+}
