3fd36092e573f90d98a4eafce01e360787fcc0cf
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-07-25 16:54:58 +0100

    Linking MongoDB using Mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// now we need to provide a connection to the database which is currently working on our local server and therefore we say local host
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to Mond DB"));
+
+db.once('open', function(){
+    console.log('Connected to database :: Mongo DB');
+});
+
+module.exports=db;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db=require('./config/mongoose');
 
 // we need to tell the index from where to get the static files i.e. the css, js and images file
 app.use(express.static('./assets'));
