a6a25779e57388fa63dfd8f07e77b96be715def2
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-08-07 13:00:59 +0100

    create schema for posts

---
+++ models/post.js
@@ -0,0 +1,24 @@
+// import mongoose
+const mongoose = require('mongoose');
+
+// for creating a schema
+const postSchema = new mongoose.Schema({
+    // this schema will have the following fields
+    content : {
+        type : string,
+        required : true
+    },
+    user :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User'
+    }
+},
+{
+    // add timestamps
+    timestamps : true
+});
+
+// now we need to export this but before that we need to tell that it is a model
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
