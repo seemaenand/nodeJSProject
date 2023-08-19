c347d0e751b5b36763cfe29945c0d753859eaaa1
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-08-19 20:06:24 +0530

    deleting a post

--- ...routes/posts.js
+++ ...routes/posts.js
@@ -5,5 +5,7 @@ const passport = require('passport');
 const postsController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthentication, postsController.create);
+// create a route for deleting comments and posts
+router.get('/destroy/.id', passport.checkAuthentication, postsController.destroy);
 
 module.exports=router;
